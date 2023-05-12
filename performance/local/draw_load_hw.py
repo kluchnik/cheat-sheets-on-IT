83073060a4399562bc80d0da4c3a5f9d97ec0c51
Kluchnik <44801636+kluchnik@users.noreply.github.com>
2023-05-12 17:54:38 +0300

    Update /performance/local/draw_load_hw.py
    Add draw level

--- performance/local/draw_load_hw.py
+++ performance/local/draw_load_hw.py
@@ -9,43 +9,49 @@ def get_args(argv) -> tuple:
   ''' Возвращает значение аргументов '''
   input_f = '/var/log/load_hw.csv'
   output_f = '/tmp/foo.png'
-  opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  level = 3
+  opts, args = getopt.getopt(argv,"hi:o:l:",["ifile=","ofile=","level="])
   for opt, arg in opts:
     if opt == '-h':
-      print ('draw_load_hw.py -i <input_file> -o <output_file>')
+      print ('draw_load_hw.py -i <input_file> -o <output_file> -l <draw_level>')
       sys.exit()
     elif opt in ("-i", "--ifile"):
       input_f = arg
     elif opt in ("-o", "--ofile"):
       output_f = arg
-  return input_f, output_f
+    elif opt in ("-l", "--level"):
+      level = arg
+  return input_f, output_f, level
     
 def main(argv):
-  input_f, output_f = get_args(argv)
+  input_f, output_f, level = get_args(argv)
   # Import Data
   df = pd.read_csv(input_f)
   x = df['time']
   y1 = df['cpu_%']
   y2 = df['mem_MB']
   y3 = df['disk_MB']
-  
+ 
   # Plot Line1 (Left Y Axis)
   fig, ax1 = plt.subplots(1,1,figsize=(16,9), dpi= 80)
   ax1.plot(x, y1, color='tab:red')
   ax1.ticklabel_format(style='plain', useOffset=False, axis='y')
   
   # Plot Line2 (Right Y Axis)
-  ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-  ax2.plot(x, y2, color='tab:blue')
-  ax2.ticklabel_format(style='plain', useOffset=False, axis='y')
-  
+  if level >= 2:
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.plot(x, y2, color='tab:blue')
+    ax2.ticklabel_format(style='plain', useOffset=False, axis='y')
+
   # Plot Line3 (Right Y Axis)
-  ax3 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-  ax3.spines.right.set_position(('axes', 1.1))
-  # old version matplotlib:
-  # ax3.spines['right'].set_position(('axes', 1.1))
-  ax3.plot(x, y3, color='tab:green')
-  ax3.ticklabel_format(style='plain', useOffset=False, axis='y')
+  if level >= 3:
+    ax3 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    # new version matplotlib:
+    ax3.spines.right.set_position(('axes', 1.1))
+    # old version matplotlib:
+    # ax3.spines['right'].set_position(('axes', 1.1))
+    ax3.plot(x, y3, color='tab:green')
+    ax3.ticklabel_format(style='plain', useOffset=False, axis='y')
   
   # Decorations X axis
   ax1.set_xlabel('time', fontsize=20)
@@ -54,11 +60,13 @@ def main(argv):
   ax1.set_ylabel('cpu, %', color='tab:red', fontsize=20)
   ax1.tick_params(axis='y', rotation=0, labelcolor='tab:red', labelsize=12)
   # Decorations right Y2 axis
-  ax2.set_ylabel('memory, MB', color='tab:blue', fontsize=20)
-  ax2.tick_params(axis='y', labelcolor='tab:blue', labelsize=12)
+  if level >= 2:
+    ax2.set_ylabel('memory, MB', color='tab:blue', fontsize=20)
+    ax2.tick_params(axis='y', labelcolor='tab:blue', labelsize=12)
   # Decorations right Y3 axis
-  ax3.set_ylabel('disk, MB', color='tab:green', fontsize=20)
-  ax3.tick_params(axis='y', labelcolor='tab:green', labelsize=12)
+  if level >= 3:
+    ax3.set_ylabel('disk, MB', color='tab:green', fontsize=20)
+    ax3.tick_params(axis='y', labelcolor='tab:green', labelsize=12)
   
   # Decorations
   ax1.set_title("Load HW", fontsize=22)

f2bbc74d8d4b0cebed8215ec52aea2fb42f1d8aa
Kluchnik <44801636+kluchnik@users.noreply.github.com>
2023-05-15 23:03:24 +0300

    Update /performance/local/draw_load_io_disk.py

--- performance/local/draw_load_io_disk.py
+++ performance/local/draw_load_io_disk.py
@@ -9,7 +9,7 @@ def get_args(argv) -> tuple:
   input_f = '/var/log/load_io_disk.log'
   output_f = '/tmp/foo.png'
   disk_name = 'sda'
-  level = 4
+  level = 3
   opts, args = getopt.getopt(argv,"hi:o:d:l:",["ifile=","ofile=","disk_name="])
   for opt, arg in opts:
     if opt == '-h':
@@ -33,28 +33,38 @@ def read_log_file(input_f:str, disk_name:str) -> tuple:
   util_pct = np.array([])
 
   with open(input_f, "r") as file:
+    tmp_time = []
     for line in file:
       find_data_time = re.findall(r'^\d+/\d+/\d+ \d+:\d+:\d+', line.strip())
-      find_metering = re.findall(r'^{}.*'.format(disk_name), line.strip())
-      if find_data_time:
+      if find_data_time != tmp_time:
         tmp_time = find_data_time
-      if find_metering:
+	find_header = []
+	find_metering = []
+      else:
+        find_header = re.findall(r'^Device.*', line.strip())
+        find_metering = re.findall(r'^{}.*'.format(disk_name), line.strip())
+      if find_metering and find_header:
+        header = re.sub(r'[ ]+', ',', find_header[0]).split(',')
+	number_r_MBps = header.index('rMB/s')
+	number_w_MBps = header.index('wMB/s')
+	number_svctm_ms = header.index('svctm')
+	number_util_pct = header.index('%util')
         metering = find_metering[0]
         metering = re.sub(r',', '.', metering)
         metering = re.sub(r'[ ]+', ',', metering)
         metering = metering.split(',')
         if tmp_time:
           time = np.append(time, tmp_time[0].split(' ')[1])
-          r_MBps = np.append(r_MBps, metering[5])
-          w_MBps = np.append(w_MBps, metering[6])
-          svctm_ms = np.append(svctm_ms, metering[12])
-          util_pct = np.append(util_pct, metering[13])
+          r_MBps = np.append(r_MBps, metering[number_r_MBps])
+          w_MBps = np.append(w_MBps, metering[number_w_MBps])
+          svctm_ms = np.append(svctm_ms, metering[number_svctm_ms])
+          util_pct = np.append(util_pct, metering[number_util_pct])
   
   return (time,
-    util_pct.astype(np.float64),
-	r_MBps.astype(np.float64),
-	w_MBps.astype(np.float64),
-	util_pct.astype(np.float64))
+          util_pct.astype(np.float64),
+          r_MBps.astype(np.float64),
+          w_MBps.astype(np.float64),
+          util_pct.astype(np.float64))
 
 def main(argv):
   input_f, output_f, disk_name, level = get_args(argv)
